0e11269ed0c3d52a08b4feea06cd8b3a42881f83
lemejiamo <luismejia69@gmail.com>
2021-11-29 22:06:55 -0500

    learning to concat strings

---
+++ 0x00-csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str1 = "Holberton";
+		string str2 = "School";
+		Console.Write("Welcome to {0} ", str1);
+		Console.WriteLine("{0}!", str2);
+	}
+}
