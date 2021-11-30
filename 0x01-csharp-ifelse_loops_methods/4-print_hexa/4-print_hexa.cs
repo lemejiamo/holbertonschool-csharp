70e465c6a1d5c3a3847290837a0ca8259dd0ffed
lemejiamo <luismejia69@gmail.com>
2021-11-30 17:57:48 -0500

    print number in hexa base

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dec_num = 0;
+            for (; dec_num < 99; dec_num++)
+            {
+                string hex = dec_num.ToString("X");
+                Console.WriteLine($"{dec_num} = 0x{hex}");
+            }
+
+        }
+    }
+}
