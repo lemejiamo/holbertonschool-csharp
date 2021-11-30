23f8f6c00ce7d6d54554d519c588dd69a5599c46
lemejiamo <luismejia69@gmail.com>
2021-11-29 21:36:11 -0500

    print float in C#

---
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -0,0 +1,9 @@
+﻿using System;
+    class Program
+{
+static void Main(string[] args)
+    {
+        float number = 3.14159f;
+        Console.WriteLine("Float: {0:0.00}", number); // '0.00' represent the number of decimals to print
+    }
+}
