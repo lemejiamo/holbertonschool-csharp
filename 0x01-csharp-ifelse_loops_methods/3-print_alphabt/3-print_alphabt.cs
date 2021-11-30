8eab7541764dac7510799d4a84aae504308a5184
lemejiamo <luismejia69@gmail.com>
2021-11-30 17:01:15 -0500

    print alphabet with exceptions

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Char letter;
+            for (letter = 'a'; letter <= 'z'; letter++)
+            {
+                if (letter != 'q' && letter != 'e')
+                {
+                Console.Write($"{letter}");
+                }
+            }
+        }
+    }
+}
