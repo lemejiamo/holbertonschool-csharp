6ccbba0119b76ec8936265dca2165f57e67cf27e
lemejiamo <luismejia69@gmail.com>
2021-11-30 16:54:54 -0500

    print the alphabet with a loop

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Char letter;
+            for (letter = 'a'; letter <= 'z'; letter++)
+            {
+                Console.Write($"{letter}");
+            }
+        }
+    }
+}
