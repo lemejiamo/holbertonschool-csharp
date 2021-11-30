ea2f0bc78b0b0a013f6d6f4a12a77cc1ba96cf08
lemejiamo <luismejia69@gmail.com>
2021-11-29 23:36:10 -0500

    printint to stderr

--- 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        //string error = "and that piece of art is useful - Dora Korpar, 2015-10-19";
-        Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+        string error = "and that piece of art is useful - Dora Korpar, 2015-10-19";
+        Console.Error.WriteLine($"{error}");
         Environment.Exit(1);
     }
 }
