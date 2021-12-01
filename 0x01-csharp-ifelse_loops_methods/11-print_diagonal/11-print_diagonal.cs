4cb98ef5600df4d789b7977fd5b14a68aa14f0e4
lemejiamo <luismejia69@gmail.com>
2021-11-30 22:26:34 -0500

    method to print a diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int i = 0;
+        string line = "";
+
+
+        for(; i < length; i++)
+        {
+            int spaces = i;
+
+            line = "";
+
+            for ( ; spaces > 0; j--)
+                line += " ";
+
+            line += "\\";
+            Console.WriteLine(line);
+        }
+        Console.WriteLine();
+    }
+}
