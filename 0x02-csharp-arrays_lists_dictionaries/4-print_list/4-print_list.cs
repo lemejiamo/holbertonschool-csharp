e6852f288a4382edd1c8b9e86561bf8f813391b4
lemejiamo <luismejia69@gmail.com>
2021-12-01 17:00:43 -0500

    print list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int>  _list = new List<int>();
+        int i = 0;
+        if (size <= 0)
+        {
+            Console.WriteLine();
+            return _list;
+        }
+        do
+        {
+            _list.Add(i);
+            Console.Write("{0}{1}", i, i == (size - 1) ? "\n" : " ");
+            i++;
+        }while(i < size);
+
+        return _list;
+    }
+}
