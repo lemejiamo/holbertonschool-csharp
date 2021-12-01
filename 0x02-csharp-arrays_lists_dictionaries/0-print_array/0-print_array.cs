592abaecae115ed6bc7807997ec652867f8e9a60
lemejiamo <luismejia69@gmail.com>
2021-12-01 15:02:57 -0500

    Method to print a array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+
+        if (size == 0)
+            Console.WriteLine();
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = i;
+            Console.Write("{0}{1}", i, i == (size - 1)? "\n" : " ");
+        }
+        // Console.WriteLine("Array Length: {0}", size);
+        return array;
+    }
+}
