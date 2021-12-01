4974e0e36ef7197c39957160c3387cf5dae25740
lemejiamo <luismejia69@gmail.com>
2021-11-30 19:36:34 -0500

    print integers version 2

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num_1 = 0;
+            int num_2 = 0;
+            string line = "";
+            for(;num_1 < 10; num_1++)
+            {
+                num_2 = num_1 + 1;
+                for (; num_2 < 10; num_2++)
+                {
+                    if (num_1 == 8 && num_2 == 9)
+                        line +=num_1.ToString("d") + num_2.ToString("d");
+                    else
+                    {
+                        line +=num_1.ToString("d") + num_2.ToString("d") + "," + " ";
+                    }
+                }
+            }
+            Console.WriteLine(line);
+
+        }
+    }
+}
