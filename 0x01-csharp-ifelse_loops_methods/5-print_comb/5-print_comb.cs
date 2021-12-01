f2e9720d167c471b6f8ab1bf99a008c775321cce
lemejiamo <luismejia69@gmail.com>
2021-11-30 19:20:30 -0500

    print the numbers

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "";
+            for (int num = 0; num < 100; num++)
+            {
+                if (num < 10)
+                {
+                    cadena += '0' + num.ToString("d") + ',' +  ' ';
+                }
+                else if (num == 99)
+                {
+                    cadena +=  num.ToString("d");
+                }
+                else
+                {
+                    cadena += num.ToString("d") + ',' +  ' ';
+                }
+
+            }
+            Console.Write(cadena);
+
+        }
+    }
+}
