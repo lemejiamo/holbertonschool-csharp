bae31b41dfa94ac33e2810584f5ae9bb4f2693b5
lemejiamo <luismejia69@gmail.com>
2021-12-01 16:08:28 -0500

    replace element of array in a given position

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -5,7 +5,7 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        if (index < array.Length)
+        if (index < array.Length && index > 0)
         {
             array[index] = n;
             return array;
