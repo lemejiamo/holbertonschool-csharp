456a8c445328d52a35ff85670a8051ff8b8ad7cc
lemejiamo <luismejia69@gmail.com>
2021-11-30 21:49:35 -0500

    mthod to print a line

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        string line = "";
+        for (; length > 0; length--)
+        {
+            line += "_";
+        }
+        Console.WriteLine(line);
+    }
+}
