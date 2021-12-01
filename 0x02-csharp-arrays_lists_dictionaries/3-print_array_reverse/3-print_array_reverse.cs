79deae81b81aa513b1a40391295623480f23f459
lemejiamo <luismejia69@gmail.com>
2021-12-01 16:21:24 -0500

    print array in reverse

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            int len = (array.Length - 1);
+            for (;len >= 0; len--)
+            {
+                Console.Write("{0}{1}", array[len], len == 0 ? "" : " " );
+            }
+        }
+    }
+}
