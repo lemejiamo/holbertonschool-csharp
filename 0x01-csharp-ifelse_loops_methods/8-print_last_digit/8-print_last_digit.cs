4213e28df7554a8070c1865fdcfea80c561e849a
lemejiamo <luismejia69@gmail.com>
2021-11-30 21:34:00 -0500

    print last digit method

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        if (number < 0)
+            number = number * -1;
+        int last_digit = number % 10;
+        do
+        {
+            if (last_digit > 9)
+            {
+                last_digit = last_digit % 10;
+            }
+        }while (last_digit > 9);
+        Console.Write(last_digit);
+        return last_digit;
+    }
+}
