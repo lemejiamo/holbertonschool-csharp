fd9f2945254a89ce77de95b8d7fe2a62180d6dd7
lemejiamo <luismejia69@gmail.com>
2021-12-02 14:08:33 -0500

    method that returns a sorted list of all elements present in one or the other list but not both.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> return_list = new List<int>();
+
+        foreach(int num in list1)
+        {
+            if(list2.Contains(num) == false)
+            {
+                return_list.Add(num);
+            }
+        }
+
+        foreach(int num in list2)
+        {
+            if(list1.Contains(num) == false)
+            {
+                return_list.Add(num);
+            }
+        }
+
+        return return_list;
+    }
+}
