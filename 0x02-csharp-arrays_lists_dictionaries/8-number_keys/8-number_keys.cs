b01d1422c3763d056d0f184af3af54224309b0fd
lemejiamo <luismejia69@gmail.com>
2021-12-01 20:58:05 -0500

    number of keys in dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int number_keys = 0;
+        foreach (KeyValuePair<string, string> pair in myDict)
+        {
+            number_keys++;
+        }
+
+        return number_keys;
+    }
+}
