febfbd4bdbd4ebea8390d0cbb5c8540caf7dbeaf
lemejiamo <luismejia69@gmail.com>
2021-12-01 22:03:25 -0500

    print sorted dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        foreach(KeyValuePair<string, string> entry in myDict.OrderBy(key => key.Key))
+        {
+            Console.WriteLine($"{entry.Key}: {entry.Value}");
+        }
+
+    }
+}
