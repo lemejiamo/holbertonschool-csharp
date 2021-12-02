5afdffd9730f64e146f68e6e4dff1ff67d6ad3ae
lemejiamo <luismejia69@gmail.com>
2021-12-01 22:49:08 -0500

    rectangular array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix  = new int[5 , 5];
+            matrix[2,2] = 1;
+
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if(j == 2 && i == 2 )
+                        matrix[i,j] = 1;
+                    matrix[i,j] = 0;
+                    Console.Write("{0}", j < 5-1 ? matrix[i, j] : matrix[i,j] +"\n");
+                }
+            }
+
+        }
+    }
+}
