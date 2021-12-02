bec2231f131fbfb51c27302d4790e5c9769b770c
lemejiamo <luismejia69@gmail.com>
2021-12-02 13:56:25 -0500

    method that returns a sorted list of common elements in two lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> return_list = new List<int>();
+        
+        foreach(int num in list1)
+        {
+            if(list2.Contains(num))
+            {
+                return_list.Add(num);
+            }
+        }
+
+        return return_list;
+    }
+}
