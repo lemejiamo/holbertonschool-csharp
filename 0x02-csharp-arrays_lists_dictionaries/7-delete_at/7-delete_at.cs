6f2ebebd37a44f13620447fc426b9a67e8f1e019
lemejiamo <luismejia69@gmail.com>
2021-12-01 20:39:10 -0500

    delete at index

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        int len = myList.Count;
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return null;
+        }
+        else if (index > len)
+        {
+            Console.WriteLine("Index is out of range");
+            return null;
+        }
+        myList.Remove(index);
+
+        return myList;
+    }
+}
