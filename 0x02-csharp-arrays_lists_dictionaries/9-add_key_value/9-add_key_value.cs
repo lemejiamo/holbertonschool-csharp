f92f4e83f7bdf0eca272396a49ee9a0050f4043c
lemejiamo <luismejia69@gmail.com>
2021-12-01 21:13:42 -0500

    add key value to a dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        try
+        {
+            myDict.Add(key, value);
+        }
+        catch (ArgumentException)
+        {
+            myDict[key] = value;
+        }
+        return myDict;
+    }
+}
