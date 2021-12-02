2801fcfa4ae06cdabbfed25d9fda69426f064ccf
lemejiamo <luismejia69@gmail.com>
2021-12-01 23:07:16 -0500

    square matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int rows = myMatrix.GetLength(0);
+        int columns = myMatrix.GetLength(1);
+        int[,] new_Array = new int[rows, columns];
+
+        for(int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                new_Array[i,j] = myMatrix[i,j] *  myMatrix[i,j];
+            }
+        }
+        return new_Array;
+
+    }
+}
