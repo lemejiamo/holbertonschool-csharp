2f3735377251600f988672505c5064d53ccfa524
lemejiamo <luismejia69@gmail.com>
2021-12-01 22:14:12 -0500

    best sctore

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int max = 0;
+        string best = "";
+
+        if (myList == null)
+            return "None";
+
+        foreach(KeyValuePair<string, int> entry in myList)
+        {
+            if (entry.Value > max)
+            {
+                max = entry.Value;
+                best = entry.Key;
+            }
+        }
+            return best;
+
+    }
+}
