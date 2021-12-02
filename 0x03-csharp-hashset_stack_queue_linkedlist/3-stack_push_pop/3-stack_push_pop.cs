f43e64c3b91aebeecaf8edcd514fc27887a332db
lemejiamo <luismejia69@gmail.com>
2021-12-02 18:16:19 -0500

    retrives the info of a stack

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -3,33 +3,27 @@ using System.Collections.Generic;
 
 class MyStack
 {
-    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
-    {
-        int number_of_elements = aStack.Count;
-        string top_element;
-        string contains_element = "False";
-        top_element = number_of_elements == 0 ? "Stack is empty" : "Top item: " + aStack.Peek();
-
-        if (aStack.Contains(search))
-        {
-            while(aStack.Peek() != search)
-            {
-                aStack.Pop();
-            }
-            aStack.Pop();
-            contains_element = "True";
-        }
-
-        aStack.Push(newItem);
-
-        Console.WriteLine("Number of items: {0}", number_of_elements);
-        Console.WriteLine("{0}", top_element);
-        Console.WriteLine("Stack contains {0}: {1}", search, contains_element);
-        return aStack;
-    }
-
-    private string GetDebuggerDisplay()
-    {
-        return ToString();
-    }
+	public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+	{
+		Console.WriteLine("Number of items: {0}", aStack.Count);
+		if (aStack.Count <= 0)
+		{
+			Console.WriteLine("Stack is empty");
+		}
+		else
+		{
+			Console.WriteLine("Top item: {0}", aStack.Peek());
+		}
+		if (aStack.Contains(search))
+		{
+			Console.WriteLine("Stack contains \"{0}\": True", search);
+			while (aStack.Pop() != search) ;
+		}
+		else
+		{
+			Console.WriteLine("Stack contains \"{0}\": False", search);
+		}
+		aStack.Push(newItem);
+		return aStack;
+	}
 }
