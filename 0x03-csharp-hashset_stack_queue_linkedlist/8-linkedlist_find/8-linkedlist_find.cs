355299d2fa495f188416e998f16f6ced7542e36e
lemejiamo <luismejia69@gmail.com>
2021-12-02 23:54:23 -0500

    find a value in a llist

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+	{
+        int i = 0;
+
+        foreach(int num in myLList)
+        {
+            if (num == value)
+                return i;
+            i++;
+        }
+        return -1;
+    }
+}
