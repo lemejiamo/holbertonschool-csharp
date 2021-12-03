152cb082f7a856485d33dcc8d31f8df082773e26
lemejiamo <luismejia69@gmail.com>
2021-12-02 21:07:20 -0500

    create and print linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedList<int> CreatePrint(int size)
+	{
+        int i = 0;
+        
+		LinkedList<int> linked_list = new LinkedList<int>();
+		if (size <= 0)
+		{
+			return linked_list;
+		}
+		while(i < size)
+		{
+			Console.WriteLine(i);
+			linked_list.AddLast(i);
+            i++;
+		}
+		return linked_list;
+	}
+}
