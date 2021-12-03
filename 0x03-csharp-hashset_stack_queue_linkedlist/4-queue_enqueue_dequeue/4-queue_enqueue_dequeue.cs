2b73bedf3e507d0cbea801bf90e9cd7a834def50
lemejiamo <luismejia69@gmail.com>
2021-12-02 19:37:23 -0500

    method to enqueue dequeue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+	public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+	{
+        int len_queue = aQueue.Count;
+        string status = "False";
+        string line = "";
+        Console.WriteLine("{0}", line ="Number of items:"+len_queue.ToString("d"));
+
+        if (len_queue > 0)
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        else
+            Console.WriteLine("Queue is empty");
+
+        if (aQueue.Contains(search))
+        {
+            status = "True";
+            Console.WriteLine($"Queue contains {search}: {status}");
+            while(aQueue.Dequeue() != search) ;
+        }
+        else
+            Console.WriteLine($"Queue contains {search}: {status}");
+
+        aQueue.Enqueue(newItem);
+
+        return aQueue;
+	}
+}
