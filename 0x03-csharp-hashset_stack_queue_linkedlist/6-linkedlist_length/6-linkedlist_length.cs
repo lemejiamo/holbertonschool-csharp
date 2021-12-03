2a7f46ab10657ba85532c2bfd195f10a415b4f07
lemejiamo <luismejia69@gmail.com>
2021-12-02 21:19:20 -0500

    measure linked list size

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int Length(LinkedList<int> myLList)
+	{
+        int i = 0;
+        
+		LinkedList<int> linked_list = new LinkedList<int>();
+        foreach(int node in myLList)
+        {
+            i++;
+        }
+        return i;
+    }
+}
