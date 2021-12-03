576d308e20250c850c9336c063538285f262cec1
lemejiamo <luismejia69@gmail.com>
2021-12-02 19:49:34 -0500

    method to enqueue dequeue

--- 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -1,27 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-
 class MyQueue
 {
 	public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
 	{
         Console.WriteLine("Number of items: {0}", aQueue.Count);
 
-        if ( aQueue.Count > 0)
+        if (aQueue.Count > 0)
             Console.WriteLine("First item: {0}", aQueue.Peek());
         else
             Console.WriteLine("Queue is empty");
 
         if (aQueue.Contains(search))
         {
-            Console.WriteLine($"Queue contains {search}: True");
+            Console.WriteLine($"Queue contains \"{search}\": True");
             while(aQueue.Dequeue() != search) ;
         }
         else
-            Console.WriteLine($"Queue contains {search}: False");
+            Console.WriteLine($"Queue contains \"{search}\": False");
 
         aQueue.Enqueue(newItem);
-
         return aQueue;
 	}
 }
