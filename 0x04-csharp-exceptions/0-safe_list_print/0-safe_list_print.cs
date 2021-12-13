42a5605c1ba02caabc47dc0bf5f8c0b0473499a3
debian <debian@debian>
2021-12-14 00:20:14 +0100

    safe list print

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int toPrint = n;
+        int printed = 0;
+        try
+        {
+            for(;  printed < toPrint; printed++)
+            {
+                Console.WriteLine(myList[printed]);
+            }
+        }
+        catch
+        {
+
+        }
+        return (printed);
+    }
+}
