5e0137ee11cd5ae73ad779fcbcb3edf30780c4bf
debian <debian@debian>
2021-12-14 01:05:28 +0100

    divide_print

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int result = 0;
+
+        try
+        {
+            result =  a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("{1} // {2} = {3}", a, b, result);
+        }
+    }
+}
