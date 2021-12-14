c5e416908c28421a637a97087d21aca2f5842d9e
debian <debian@debian>
2021-12-14 01:37:26 +0100

    divide_list

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> results = new List<int>();
+        try
+        {
+            int index = 0;
+
+            for (; index < listLength; index++) 
+            {
+                int result = list1[index] / list2[index];
+                results.Add(result);
+            }
+        }
+        catch(DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            results.Add(0);
+        }
+        catch(System.ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Out of range");
+        }
+        return results;
+    }
+}
+
