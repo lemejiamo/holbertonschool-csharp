379f5645c18228a2f9fafcdeee116c058299a883
debian <debian@debian>
2021-12-14 04:19:22 +0100

    override the .ToString() method

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+struct Dog
+{
+    public Dog (string Name, float Age, string Owner, Rating Rating)
+    {
+        rating = Rating;
+        name = Name;
+        age = Age;
+        owner = Owner;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+} 
