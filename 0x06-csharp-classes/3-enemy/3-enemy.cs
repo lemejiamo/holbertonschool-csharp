0f4d9c95622c13a861c1167d1f90737a78fd61ed
debian <debian@debian>
2021-12-15 00:57:52 +0100

    adding public methods

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }        
+        public Zombie(int value)
+        {
+            
+            if(value < 0)
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;               
+        }
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
