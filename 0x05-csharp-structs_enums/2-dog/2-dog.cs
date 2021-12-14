caea55bcda1d6b7b63db9ff31511e39ac3cc87f9
debian <debian@debian>
2021-12-14 04:14:05 +0100

    constructor to struc dog

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+struct Dog
+{
+    public Dog (string Name, float Age, string Owner, Rating Rating)
+    {
+        rating = Rating;
+        name = Name;
+        age = Age;
+        owner = Owner;
+    }
+    public string name {get; init;}
+    public float age {get; init;}
+    public string owner {get; init;}
+    public Rating rating {get; init;}
+} 
