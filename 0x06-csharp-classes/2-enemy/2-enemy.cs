4bb5ef78984705ba197b3af0d9415819a3a99af9
debian <debian@debian>
2021-12-15 00:40:33 +0100

    learning multiple constructors

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }        
+        public Zombie(int value)
+        {
+            
+            if(value < 0)
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+        }
+    }
+}
