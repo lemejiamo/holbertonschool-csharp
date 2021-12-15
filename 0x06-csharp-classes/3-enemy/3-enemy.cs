440b7c61e7520ee2659c6b54368e073cae40e666
debian <debian@debian>
2021-12-15 04:25:21 +0100

    added XML documentation

--- 0x06-csharp-classes/3-enemy/3-enemy.cs
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -2,21 +2,39 @@
 
 namespace Enemies
 {
+    /// <sumary>
+    /// empty class Zombie
+    /// </sumary>
     public class Zombie
     {
+        /// <summary>
+        /// Field Zombie Health .
+        /// </summary>
         public int health;
+
+        /// <summary>
+        /// constructor for a Zombie.
+        /// </summary>
         public Zombie()
         {
             health = 0;
-        }        
+        }
+
+        /// <summary>
+        /// constructor for a Zombie with Health args.
+        /// </summary>
         public Zombie(int value)
         {
-            
+
             if(value < 0)
                 throw new System.ArgumentException("Health must be greater than or equal to 0");
             else
-                health = value;               
+                health = value;
         }
+
+        /// <summary>
+        /// Method to get Healt stat from a zombie
+        /// </summary>
         public int GetHealth()
         {
             return health;
