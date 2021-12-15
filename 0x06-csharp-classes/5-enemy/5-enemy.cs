62e510686d9cc5b453e15bd1121bcbc6b3cadadf
debian <debian@debian>
2021-12-15 01:44:16 +0100

    override .toString() method

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public int health;
+        private string name = "(No name)";
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if(value < 0)
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+        public int GetHealth()
+        {
+            return health;
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public override string ToString()
+        {
+            return ($"Zombie Name: {name} / Total Health: {health}");
+        }
+    }
+}
