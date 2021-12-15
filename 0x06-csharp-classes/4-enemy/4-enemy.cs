8dca9dfb1d88948aa12d577f34c05efeefed6b17
debian <debian@debian>
2021-12-15 01:24:32 +0100

    adding a property

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public int health;
+        private string name = "(No name)";
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if(value < 0)
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            else
+                health = value;
+        }
+        public int GetHealth()
+        {
+            return health;
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+    }
+}
