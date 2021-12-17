9523fcfbc60c907c3d2d28c63239feb9457e7f00
debian <debian@debian>
2021-12-17 02:31:58 +0100

    First test module

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class SumTest
+    {
+        [Test]
+        public void sum()
+        {
+            int a = 3;
+            int b = 2;
+            Assert.AreEqual(5, MyMath.Operations.Add(a, b));
+
+        }
+
+        [Test]
+        public void negativesum()
+        {
+            int a = -4;
+            int b = -15;
+            Assert.AreEqual(-9, MyMath.Operations.Add(a, b));
+        }
+
+        [TestCase(0, 1)]
+        [TestCase(1, -9)]
+        [TestCase(3, 6)]
+        [TestCase(-4, 4)]
+        [TestCase(-5, 0)]
+        [TestCase(-3, -2)]
+        [TestCase(0, 0)]
+        public void integers(int a, int b)
+        {
+            Assert.AreEqual(a + b, MyMath.Operations.Add(a, b));
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MyMath
+{
+    public static int Add(int a, int b)
+    {
+        return a+b;
+    }
+}
