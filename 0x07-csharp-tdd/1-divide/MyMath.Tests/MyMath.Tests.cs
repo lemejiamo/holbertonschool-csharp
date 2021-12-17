36a086aa0ac6bcf164564b28c51b191c67fea42f
debian <debian@debian>
2021-12-17 04:17:18 +0100

    tests

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -22,5 +22,22 @@ namespace MyMath.Tests
             int [,] expected = new int[2,2]{{1,8},{2,5},{3, 9},{7, 10}};
             Assert.AreEqual(expected, results);
         }
+        [Test]
+        public void matrix()
+        {
+            int[,] firstMatrix = null;
+
+            int[,] newMatrix = Matrix.Divide(firstMatrix, 5);
+            Assert.AreEqual(null, newMatrix);
+        }
+
+        [Test]
+        public void divide()
+        {
+            int[,] firstMatrix = new int[2, 2] {{4, 2}, {2, 4}};
+
+            int[,] newMatrix = Matrix.Divide(firstMatrix, 2);
+            Assert.AreEqual(new int[2, 2] {{2, 1}, {2, 1}}, newMatrix);
+        }
     }
 }
