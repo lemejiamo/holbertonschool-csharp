7ee27d73e04bf16581781bc2bd989f8e9f19bc84
debian <debian@debian>
2021-12-20 18:48:59 +0100

    method to verify if object is 'int' type

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Obj
+{
+    /// <summary>
+    /// static method to veryfy if object is type of 'int'
+    /// </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+            return true;
+        else
+            return false;
+    }
+}
+
