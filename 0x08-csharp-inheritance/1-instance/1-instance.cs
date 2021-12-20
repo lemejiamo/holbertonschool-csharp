e7574478f5d1e2bbefda871c8916ca756cc13010
debian <debian@debian>
2021-12-20 23:55:10 +0100

    is instance of array

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+/// <summary>
+/// static method to veryfy if object is type of 'Array'
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// static method to veryfy if object is type of 'Array'
+    /// </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        Console.WriteLine("{0}", Type.GetTypeArray(obj));
+        if(typeof(Array).IsInstanceOfType(obj))
+            return true;
+        return false;
+    }
+}
+
