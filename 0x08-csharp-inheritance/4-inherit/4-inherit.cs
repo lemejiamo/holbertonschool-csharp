c5ce434b010578acde95de95d431b1c2617d9b7d
debian <debian@debian>
2021-12-21 03:14:37 +0100

    fixed comentary label

--- 0x08-csharp-inheritance/4-inherit/4-inherit.cs
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -1,12 +1,16 @@
 using System;
 
-/// <summary>empty class animal </summary>
+/// <summary>
+/// empty class animal
+/// </summary>
 class Animal
 {
-    ///empty class
+
 }
-/// <summary>empty class dog</summary>
+/// <summary>
+/// empty class dog
+/// </summary>
 class Dog : Animal
 {
-    ///empty class
+
 }
