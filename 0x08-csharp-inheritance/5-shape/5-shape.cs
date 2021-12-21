c038766424d63bec4d2debb6646cbcac027aad49
debian <debian@debian>
2021-12-21 03:21:23 +0100

    create a virtual method

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+using System;
+/// <summary>
+/// empty class Shape
+/// </summary>
+class Shape
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
