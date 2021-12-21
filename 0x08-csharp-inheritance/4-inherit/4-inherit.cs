727dab4ae5c4eefa1a006d51cf905eb904cdfa5d
debian <debian@debian>
2021-12-21 03:06:37 +0100

    inheritance

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,11 @@
+using System;
+/// <summary>empty class animal </summary>
+class Animal
+{
+    ///empty class
+}
+/// <summary>empty class dog</summary>
+class Dog : Animal
+{
+    ///empty class
+}
