03753035593b033b2961e476b07f33c0f6652655
debian <debian@debian>
2021-12-21 05:15:10 +0100

    square inherit from rectangle

---
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,93 @@
+using System;
+
+/// <summary>a class Shape.</summary>
+class Shape
+{
+    /// <summary>public method: public virtual int Area()</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>a class Rectangle that inherits from Shape</summary>
+class Rectangle : Shape {
+    ///<summary>private int width </summary>
+    private int width;
+
+    ///<summary>private int height </summary>
+    private int height;
+
+    /// <summary>public int Width</summary>
+    public int Width {
+        get
+        {
+            return (width);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+
+    /// <summary>public property</summary>
+    public int Height {
+        get
+        {
+            return (height);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                height = value;
+            }
+        }
+    }
+    /// <summary>will override the Area() method defined in the Shape base class.</summary>
+    public new int Area()
+    {
+        return (height * width);
+    }
+    /// <summary>public override string ToString().</summary>
+    public override string ToString()
+    {
+        return (string.Format("[Rectangle] {0} / {1}", width, height));
+    }
+}
+
+/// <summary>Square class inherits from rectangle</summary>
+class Square : Rectangle
+{
+    /// <summary>provate attribute</summary>
+    private int size;
+
+/// <summary>public property to get anset size of a square</summary>
+    public int Size
+    {
+        get
+        {
+            return size;
+        }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            else
+            {
+                size = value;
+            }
+        }
+    }
+}
