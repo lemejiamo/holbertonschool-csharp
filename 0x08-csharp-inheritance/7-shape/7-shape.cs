a47e543171547a158a0b5a6c4d708f44b7926ecf
debian <debian@debian>
2021-12-21 05:27:49 +0100

    minor bugs

--- 0x08-csharp-inheritance/7-shape/7-shape.cs
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -1,25 +1,25 @@
 using System;
-/// <summary>
-/// empty class Shape
-/// </summary>
+
+/// <summary>a class Shape.</summary>
 class Shape
 {
+    /// <summary>public method: public virtual int Area()</summary>
     public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
     }
 }
-class Rectangle : Shape
-{
-    /// <summary>Private attribute width</summary>
+
+/// <summary>a class Rectangle that inherits from Shape</summary>
+class Rectangle : Shape {
+    ///<summary>private int width </summary>
     private int width;
 
-    /// <summary>private attribute height</summary>
+    ///<summary>private int height </summary>
     private int height;
 
-    /// <summary>property to get a set width value</summary>
-    public int Width
-    {
+    /// <summary>public int Width</summary>
+    public int Width {
         get
         {
             return (width);
@@ -36,9 +36,9 @@ class Rectangle : Shape
             }
         }
     }
-    ///<sumary>property to get and set height value</sumary>
-    public int Height
-    {
+
+    /// <summary>public property</summary>
+    public int Height {
         get
         {
             return (height);
@@ -55,14 +55,14 @@ class Rectangle : Shape
             }
         }
     }
-    /// <summary>override area method</summary>
+    /// <summary>will override the Area() method defined in the Shape base class.</summary>
     public new int Area()
     {
-        return (width * height);
+        return (height * width);
     }
-    /// <summary>override ToString method</summary>
+    /// <summary>public override string ToString().</summary>
     public override string ToString()
     {
-        return ($"[Rectangle]: {width} / {height}");
+        return (string.Format("[Rectangle] {0} / {1}", width, height));
     }
 }
