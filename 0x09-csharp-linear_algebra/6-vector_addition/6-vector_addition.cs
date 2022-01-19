d44681d0db8dc56abdb319f3a1370edfaf2b9bae
debian <debian@debian>
2022-01-19 19:51:23 +0100

    add to vectors

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _6_vector_addition
+{
+    /// <summary>
+    /// Method to adds to vector
+    /// </summary>
+    class VectorMath
+    {
+        /// <summary>
+        /// Method to adds to vectors
+        /// </summary>
+        /// <param name="vector1"></param>
+        /// <param name="vector2"></param>
+        /// <returns>return a new vector with the sum</returns>
+        public static double[] Add(double[] vector1, double[] vector2)
+        {
+            double[] vector = new double[vector1.Length];
+            int i = 0;
+
+            if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3)
+            {
+                return new double[] { -1 };
+            }
+
+            for (i=0; i < vector1.Length; i++)
+            {
+                vector[i] = vector1[i] + vector2[i];
+            }
+
+            return vector;
+        }
+    }
+}
