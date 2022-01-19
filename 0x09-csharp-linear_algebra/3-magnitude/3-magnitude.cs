7ee576ab2eae0324fb1d5ae419601e95e28533a4
debian <debian@debian>
2022-01-19 19:02:33 +0100

    method that calculates and returns the length of a given vector

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// Method that calculates and returns the length of a given vector.
+/// </summary>
+
+class VectorMath
+{
+    /// <summary>
+    /// Methot that returns a magnitud of a given 2D or 3D vector
+    /// </summary>
+    /// <param name="vector"></param>
+    /// <returns>The Vector magnitude</returns>
+
+    public static double Magnitude(double[] vector)
+    {
+        double sum = 0, magnitude = 0;
+
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            return -1;
+        }
+
+        foreach (double number in vector)
+        {
+            sum += number*number;
+        }
+        magnitude = Math.Round(Math.Sqrt(sum), 2);
+        return magnitude;
+    }
+}
+
