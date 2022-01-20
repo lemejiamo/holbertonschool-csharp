b5432da0fcfe5d3eae6c8acdc859245de58bfec7
debian <debian@debian>
2022-01-20 08:57:33 +0100

    25_matrix transpolation

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] new_matrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j=0; j<matrix.GetLength(1);j++)
+            {
+                new_matrix[j, i] = matrix[i, j];
+            }
+        }
+        return new_matrix;
+    }
+}
