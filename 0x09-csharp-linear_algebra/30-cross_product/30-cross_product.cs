b80b7dc0849d667b706703e5845c7063238a78f2
debian <debian@debian>
2022-01-21 02:29:08 +0100

    cross product

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MatrixMath 
+{
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        double[] product = new double[] { 0, 0, 0 };
+
+        if (vector1.Length != 3 || vector2.Length != 3)
+            return new double[] { -1 };
+
+        /* calculate the factors*/
+        product[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
+        product[1] = (vector1[0] * vector2[2]) - (vector1[2] * vector2[0]);
+        product[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
+
+        return product;
+    }
+}
