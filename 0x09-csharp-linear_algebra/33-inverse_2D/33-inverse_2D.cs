3f90d6643a5de30122c112ea7e6688e7fdbbd2f6
debian <debian@debian>
2022-01-21 04:48:35 +0100

    add determinant funt

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -1,7 +1,29 @@
 ﻿using System;
-
+/// <summary>
+/// 
+/// </summary>
 class MatrixMath
 {
+    public static double Determinant(double[,] matrix)
+    {
+        double det = -1;
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            det = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+            return (Math.Round(det, 2));
+        }
+        else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+        {
+            det = ((matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]))) - (matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]))) + (matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0]))));
+            return (Math.Round(det, 2));
+        }
+        else
+        {
+            return (det);
+        }
+    }
+
+    
     public static double[,] Inverse2D(double[,] matrix)
     {
         double[,] identity_Matrix = new double[,] { { 1, 0 }, { 0, 1 } };
@@ -10,6 +32,9 @@ class MatrixMath
         double[] vector2 = new double[4];
         double factor1 = matrix[1, 1];
         double factor2 = matrix[0,1];
+
+        if (Determinant(matrix) == 0)
+            return new double[,] { { -1 } };
 
         if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return new double[,] { { -1 } };
@@ -36,8 +61,6 @@ class MatrixMath
                     tem_matrix[i, j] = vector1[j] + vector2[j];
                 }
             }
-
-
         }
 
         if (matrix[1, 0] != 0)
@@ -47,7 +70,6 @@ class MatrixMath
 
             if (factor1 > 0 && factor2 > 0)
                 factor1 = factor1 * -1;
-
 
             vector1[0] = matrix[0, 0] * factor1;
             vector1[1] = matrix[0, 1] * factor1;
