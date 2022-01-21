48e27304c846a095281cd1562f8474c889a042f0
debian <debian@debian>
2022-01-21 04:45:47 +0100

    determinant

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/// <summary>
+/// Contains all the Method used to handle matrix 
+/// </summary>
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+        double det = -1;
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            det = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+            return (Math.Round(det, 2));
+        }
+        else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+        {
+            det = ((matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]))) - (matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]))) + (matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0]))));
+            return (Math.Round(det, 2));
+        }
+        else
+        {
+            return (det);
+        }
+    }
+}
